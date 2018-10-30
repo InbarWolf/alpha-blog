a0d1f71e0f61a1a22d7cb10e02191e9c90d627a7
inbarwolf@gmail.com <inbarwolf@gmail.com>
2018-10-30 11:13:55 +1100

    style error and flash notifications

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article.destroy
-        flash[:notice] = "Article was deleted!"
+        flash[:success] = "Article was deleted!"
         redirect_to articles_path
     end
 
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was saved"
+            flash[:success] = "Article was saved"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
 
     def update
         if @article.update article_params
-            flash[:notice] = "Article was updated sucessfully"
+            flash[:success] = "Article was updated sucessfully"
             redirect_to article_path(@article)
         else
             render 'edit'
